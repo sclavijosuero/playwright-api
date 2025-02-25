cd6af65972d4e008e19053f1356d40b668a59e19
Sebastian Clavijo Suero (Personal) <128645385+sclavijosuero@users.noreply.github.com>
2025-02-24 22:18:22 -0800

    Support of typescript and added tests for get, post, fetch, put, patch, delete

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,49 +1,77 @@
 import { test, expect } from '@playwright/test';
-import { apiFetch } from '../src';
-
-const baseUrl = 'https://jsonplaceholder.typicode.com';
-
-test('GET /posts/1', async ({ request, page }) => {
-  const response = await request.get(`${baseUrl}/posts/1`);
-  expect(response.status()).toBe(200);
-  const responseBody = await response.json();
-  expect(responseBody).toHaveProperty('id', 1);
-
-});
-
-test('FETCH /posts/1', async ({ request, page }) => {
-  const response = await request.fetch(`${baseUrl}/posts/1`);
-  expect(response.status()).toBe(200);
-  const responseBody = await response.json();
-  expect(responseBody).toHaveProperty('id', 1);
+import { apiFetch, apiGet, apiPost, apiPut, apiDelete, apiPatch, apiHead } from '../src/index';
 
 
-});
+test.describe('API Tests for https://jsonplaceholder.typicode.com', () => {
 
-test('APIFETCH /posts/1', async ({ request, page }) => {
-  // API Call1
-  const response = await apiFetch({ request, page }, `${baseUrl}/posts/1`);
-  expect(response.status()).toBe(200);
-  const responseBody = await response.json();
-  expect(responseBody).toHaveProperty('id', 1);
+    const baseUrl = 'https://jsonplaceholder.typicode.com';
 
-  // API Call2
-  const response2 = await apiFetch({ request, page }, `${baseUrl}/posts`, {
-    method: 'POST',
-    body: {
-      title: 'foo',
-      body: 'bar',
-      userId: 1,
-    },
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    }
-  });
-  expect(response2.status()).toBe(201);
 
-  //API Call3
-  const response3 = await apiFetch({ request, page }, `${baseUrl}/posts`);
-  expect(response3.status()).toBe(200);
-  const responseBody3 = await response3.json();
+    test('Test for apiGet, apiPost and apiFetch', async ({ request, page }) => {
+        // Example of apiGet
+        const responseGet = await apiGet({ request, page }, `${baseUrl}/posts/1`);
+        expect(responseGet.status()).toBe(200);
+        const responseBodyGet = await responseGet.json();
+        expect(responseBodyGet).toHaveProperty('id', 1);
 
-});
+
+        // Example of apiPost with request body and request headers
+        const responsePost = await apiPost({ request, page }, `${baseUrl}/posts`, {
+            body: {
+                title: 'foo',
+                body: 'bar',
+                userId: 1,
+            },
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            }
+        });
+        expect(responsePost.status()).toBe(201);
+
+        
+        // Example of apiFetch (default GET)
+        const responseFetch = await apiFetch({ request, page }, `${baseUrl}/posts`);
+        expect(responseFetch.status()).toBe(200);
+
+    })
+
+
+    test('Test for apiPut, apiPatch, and apiDelete', async ({ request, page }) => {
+        // Example of apiPut
+        const responsePut = await apiPut({ request, page }, 'https://jsonplaceholder.typicode.com/posts/1', {
+            body: {
+                id: 1,
+                title: 'foo',
+                body: 'bar',
+                userId: 1,
+            },
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        });
+        expect(responsePut.ok()).toBeTruthy();
+
+
+        // Example of apiPatch
+        const responsePatch = await apiPatch({ request, page }, 'https://jsonplaceholder.typicode.com/posts/1', {
+            body: {
+                title: 'foo',
+            },
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        });
+        expect(responsePatch.ok()).toBeTruthy();
+
+
+        // Test for apiDelete
+        const responseDelete = await apiDelete({ request, page }, 'https://jsonplaceholder.typicode.com/posts/1');
+        expect(responseDelete.ok()).toBeTruthy();
+
+    })
+
+})
+
+// test.describe('API Tests for ...', () => {
+
+// })
